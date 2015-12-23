0831e21942d5bea2f49b43b6d8bfac9c6ffd4816
Rishi Giri <rushforlinux@gmail.com>
2015-12-23 09:33:49 +0530

    Update notify.py

--- notify.py
+++ notify.py
@@ -6,7 +6,7 @@ n = notify2.Notification('Started', ' Damn! You can\'t stop it')
 n.show()
 
 while True:
-    for i in [('Thought of the Day', 'Bitch Please'), ('Thought of the Day', 'Thug Life'), ('Thought of the Day', 'Fuck Bitches')]:
+    for i in [('Thought of the Day', 'Code in Node'), ('Code in Python', 'Thug Life'), ('Do not forget to write code in Native JavaScript', 'Fuck Bitches')]:
         n.update(i[0], i[1])
         sleep(15 * 2)
         n.show()
